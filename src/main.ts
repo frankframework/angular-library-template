fe3c6735b2fd8ded44f62e90b0600b6625330430
Vivy <4380412+Matthbo@users.noreply.github.com>
2026-08-20 11:43:43 +0200

    Upgrade dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
+import { provideZoneChangeDetection } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
 function main(): void {
-  bootstrapApplication(AppComponent, appConfig).catch((error) => console.error(error));
+  bootstrapApplication(AppComponent, {
+    ...appConfig,
+    providers: [provideZoneChangeDetection(), ...appConfig.providers],
+  }).catch((error) => console.error(error));
 }
 main();
